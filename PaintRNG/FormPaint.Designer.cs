96dedea18471a258f02666d72a73f156731a54a6
Jeffery <j.c.sowinski@gmail.com>
2022-05-10 03:27:00 -0400

    Add project files.

---
+++ PaintRNG/FormPaint.Designer.cs
@@ -0,0 +1,261 @@
+﻿
+namespace PaintRNG
+{
+    partial class formPaint
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(formPaint));
+            this.pnlCanvas = new System.Windows.Forms.Panel();
+            this.btnSeed = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnPencil = new System.Windows.Forms.PictureBox();
+            this.btnEraser = new System.Windows.Forms.PictureBox();
+            this.btnBrushSize = new System.Windows.Forms.NumericUpDown();
+            this.btnColorBox = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnExitApp = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.btnPencil)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnEraser)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnBrushSize)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnColorBox)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlCanvas
+            // 
+            this.pnlCanvas.AllowDrop = true;
+            this.pnlCanvas.BackColor = System.Drawing.Color.White;
+            this.pnlCanvas.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlCanvas.Location = new System.Drawing.Point(85, 80);
+            this.pnlCanvas.Name = "pnlCanvas";
+            this.pnlCanvas.Size = new System.Drawing.Size(512, 512);
+            this.pnlCanvas.TabIndex = 0;
+            this.pnlCanvas.MouseDown += new System.Windows.Forms.MouseEventHandler(this.canvas_MouseDown);
+            this.pnlCanvas.MouseMove += new System.Windows.Forms.MouseEventHandler(this.canvas_MouseMove);
+            // 
+            // btnSeed
+            // 
+            this.btnSeed.BackColor = System.Drawing.Color.HotPink;
+            this.btnSeed.FlatAppearance.BorderSize = 0;
+            this.btnSeed.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btnSeed.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSeed.Location = new System.Drawing.Point(283, 617);
+            this.btnSeed.Name = "btnSeed";
+            this.btnSeed.Size = new System.Drawing.Size(102, 23);
+            this.btnSeed.TabIndex = 1;
+            this.btnSeed.Text = "Generate Seed";
+            this.btnSeed.UseVisualStyleBackColor = false;
+            this.btnSeed.Click += new System.EventHandler(this.btnSeed_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.Color.HotPink;
+            this.btnClear.FlatAppearance.BorderSize = 0;
+            this.btnClear.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btnClear.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClear.Location = new System.Drawing.Point(85, 615);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(102, 23);
+            this.btnClear.TabIndex = 2;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnPencil
+            // 
+            this.btnPencil.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnPencil.BackgroundImage")));
+            this.btnPencil.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnPencil.Location = new System.Drawing.Point(12, 80);
+            this.btnPencil.Name = "btnPencil";
+            this.btnPencil.Size = new System.Drawing.Size(49, 48);
+            this.btnPencil.TabIndex = 3;
+            this.btnPencil.TabStop = false;
+            this.btnPencil.Click += new System.EventHandler(this.btnPencil_Click);
+            // 
+            // btnEraser
+            // 
+            this.btnEraser.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnEraser.BackgroundImage")));
+            this.btnEraser.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnEraser.Location = new System.Drawing.Point(12, 214);
+            this.btnEraser.Name = "btnEraser";
+            this.btnEraser.Size = new System.Drawing.Size(50, 48);
+            this.btnEraser.TabIndex = 4;
+            this.btnEraser.TabStop = false;
+            this.btnEraser.Click += new System.EventHandler(this.btnEraser_Click);
+            // 
+            // btnBrushSize
+            // 
+            this.btnBrushSize.Location = new System.Drawing.Point(12, 134);
+            this.btnBrushSize.Name = "btnBrushSize";
+            this.btnBrushSize.Size = new System.Drawing.Size(49, 20);
+            this.btnBrushSize.TabIndex = 5;
+            // 
+            // btnColorBox
+            // 
+            this.btnColorBox.BackColor = System.Drawing.Color.Black;
+            this.btnColorBox.Location = new System.Drawing.Point(12, 160);
+            this.btnColorBox.Name = "btnColorBox";
+            this.btnColorBox.Size = new System.Drawing.Size(49, 48);
+            this.btnColorBox.TabIndex = 6;
+            this.btnColorBox.TabStop = false;
+            this.btnColorBox.Click += new System.EventHandler(this.btnColorBox_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Dock = System.Windows.Forms.DockStyle.None;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 24);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(61, 24);
+            this.menuStrip1.TabIndex = 14;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.BackColor = System.Drawing.Color.MistyRose;
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveToolStripMenuItem,
+            this.loadToolStripMenuItem});
+            this.fileToolStripMenuItem.ForeColor = System.Drawing.Color.HotPink;
+            this.fileToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("fileToolStripMenuItem.Image")));
+            this.fileToolStripMenuItem.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // loadToolStripMenuItem
+            // 
+            this.loadToolStripMenuItem.Name = "loadToolStripMenuItem";
+            this.loadToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.loadToolStripMenuItem.Text = "Load";
+            this.loadToolStripMenuItem.Click += new System.EventHandler(this.loadToolStripMenuItem_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.MistyRose;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.menuStrip1);
+            this.panel1.Controls.Add(this.btnExitApp);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(653, 48);
+            this.panel1.TabIndex = 15;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Franklin Gothic Demi", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.HotPink;
+            this.label1.Location = new System.Drawing.Point(3, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(247, 21);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Paint Random Number Generator";
+            // 
+            // btnExitApp
+            // 
+            this.btnExitApp.BackColor = System.Drawing.Color.MistyRose;
+            this.btnExitApp.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnExitApp.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnExitApp.FlatAppearance.BorderSize = 0;
+            this.btnExitApp.FlatAppearance.MouseOverBackColor = System.Drawing.Color.HotPink;
+            this.btnExitApp.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExitApp.ForeColor = System.Drawing.Color.MistyRose;
+            this.btnExitApp.Image = ((System.Drawing.Image)(resources.GetObject("btnExitApp.Image")));
+            this.btnExitApp.Location = new System.Drawing.Point(600, 3);
+            this.btnExitApp.Name = "btnExitApp";
+            this.btnExitApp.Size = new System.Drawing.Size(41, 31);
+            this.btnExitApp.TabIndex = 16;
+            this.btnExitApp.UseVisualStyleBackColor = false;
+            this.btnExitApp.Click += new System.EventHandler(this.btnExitApp_Click);
+            // 
+            // formPaint
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PowderBlue;
+            this.ClientSize = new System.Drawing.Size(653, 652);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnColorBox);
+            this.Controls.Add(this.btnBrushSize);
+            this.Controls.Add(this.btnEraser);
+            this.Controls.Add(this.btnPencil);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnSeed);
+            this.Controls.Add(this.pnlCanvas);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "formPaint";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "RNGPainter";
+            ((System.ComponentModel.ISupportInitialize)(this.btnPencil)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnEraser)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnBrushSize)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnColorBox)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlCanvas;
+        private System.Windows.Forms.Button btnSeed;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.PictureBox btnPencil;
+        private System.Windows.Forms.PictureBox btnEraser;
+        private System.Windows.Forms.NumericUpDown btnBrushSize;
+        private System.Windows.Forms.PictureBox btnColorBox;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadToolStripMenuItem;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnExitApp;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ PaintRNG/FormPaint.cs
@@ -0,0 +1,221 @@
+﻿// Jeffery Sowinski : April 28 2022
+// Convert pixel data into useable seed data, random number generator
+// Single form drawing space for user input of pixel data.
+// Take pixel data and convert into a useable seed form
+// through use of algorithmic hashing
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+
+// Jeffery Sowinski : April 28 2022 : Convert pixel data into useable seed data, random number generator : Single form drawing space for user input of pixel data. Take pixel data and convert into a useable seed form through use of algorithmic hashing
+
+namespace PaintRNG
+{
+    public partial class formPaint : Form
+    {
+
+        string outputPath = Path.GetDirectoryName(Application.ExecutablePath);
+
+        public Point current = new Point();
+        public Point old = new Point();
+
+        public Graphics g;
+        public Graphics graph;
+
+        public Pen pen = new Pen(Color.Black);
+
+        Bitmap surface;
+
+        public formPaint()
+        {
+            InitializeComponent();
+
+            g = pnlCanvas.CreateGraphics();
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+
+            pen.SetLineCap(System.Drawing.Drawing2D.LineCap.Round, System.Drawing.Drawing2D.LineCap.Round, System.Drawing.Drawing2D.DashCap.Round);
+
+            surface = new Bitmap(pnlCanvas.Width, pnlCanvas.Height);
+
+            graph = Graphics.FromImage(surface);
+            pnlCanvas.BackgroundImageLayout = ImageLayout.None;
+
+            pen.Width = (float)btnBrushSize.Value;
+ 
+        }
+
+        private void canvas_MouseDown(object sender, MouseEventArgs e)
+        {
+            //mouse click event location data at time of button press
+            old = e.Location;
+        }
+
+        private void canvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(e.Button == MouseButtons.Left)
+            {
+                //mouse move event location that updates based off old mouse click event location data (mouse drag)
+                current = e.Location;
+                g.DrawLine(pen, old, current);
+                graph.DrawLine(pen, old, current);
+
+                old = current;
+            }
+        }
+
+        private void btnEraser_Click(object sender, EventArgs e)
+        {
+            
+            pen.Color = Color.White;
+            pen.Width = 10;
+        }
+
+        private void btnPencil_Click(object sender, EventArgs e)
+        {
+            pen.Color = btnColorBox.BackColor;
+            pen.Width = (float)btnBrushSize.Value;
+        }
+
+        private void btnColorBox_Click(object sender, EventArgs e)
+        {
+            ColorDialog cd = new ColorDialog();
+
+            if(cd.ShowDialog() == DialogResult.OK)
+            {
+                pen.Color = cd.Color;
+                btnColorBox.BackColor = cd.Color;
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            using (graph = Graphics.FromImage(surface))
+            {
+                graph.Clear(Color.White);
+                pnlCanvas.Invalidate();
+            }
+
+            g = pnlCanvas.CreateGraphics();
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+
+            surface = new Bitmap(pnlCanvas.Width, pnlCanvas.Height);
+
+            graph = Graphics.FromImage(surface);
+            pnlCanvas.BackgroundImageLayout = ImageLayout.None;
+
+        }
+
+        private void btnSeed_Click(object sender, EventArgs e)
+        {
+            DialogResult seedMessage = MessageBox.Show("Generate new seed from pixel data and export to .txt?", "Confirmation");
+
+            if (seedMessage == DialogResult.OK)
+            {
+
+                StreamWriter sw = new StreamWriter(Path.Combine(outputPath+ "RNGoutput.txt"));
+
+                double finalTotal = 0;
+                //every pixel should now output a unique value based on its position and color data
+
+                //converts pixel color(ARGB) data into double data type, then generates a unique value per pixel based on its position and color data
+                for (int i = 0; i < surface.Width; i++)
+                {
+                    for (int j = 0; j < surface.Height; j++)
+                    {
+                        Color pixel = surface.GetPixel(i, j);
+
+                        int alpha = pixel.A;
+                        int red = pixel.R;
+                        int green = pixel.G;
+                        int blue = pixel.B;
+                        float brightness = pixel.GetBrightness();
+                        float hue = pixel.GetHue();
+                        float saturation = pixel.GetSaturation();
+
+                        //in order for x and y coordinates to generate unique values, first x has to be above the integer value 1, solution was to set it to its current value plus 1.1
+                        //to generate unique values based on y coordinate I had to take a similar approach, first I found that having the integer whole value of y be based on its position value in the grid (i.e. 1-64) it generated numbers far too large for the output and would overflow the double datatype
+                        //to fix that problem I thought to multiply the y value by 0.1 to avoid any overflow, then similar to the x coordinate, I add 0.1 to whatever value it spits out, so each iteration of the loop outputs a different value of y in order to generate unique numbers
+                        double x = (i * 0.1) + 0.2;
+                        double y = (j * 0.01) + 0.1;
+
+                        double xCoord = (x + alpha + red + green + blue);
+                        double yCoord = (brightness + hue + saturation + y);
+
+                        double function = xCoord / yCoord;
+
+                        double power = Math.Pow(function, y);
+                        
+                        // final total is the sum of all pixel values
+                        finalTotal += power;
+
+                        string readOut = power.ToString() + "\n";
+
+                        //the readout of each pixel value
+                        sw.Write(readOut);
+                    }
+                }
+                // number looks weird, because I initially started without size being a thing you can change, taking that into consideration though, I decided to divide that number by the multiple of the height and width, thus giving me more freedom to play with size and get an equivalent modulo number.
+                double modulo = finalTotal % (494975009.082627); // rather than using grid x and y, I went back to my earlier method of finding the samllest possible number the grid can generate (perform hash with white background) and using that as the modulo number
+
+
+                sw.Write(finalTotal + "\n");
+                sw.Write(modulo);
+                Console.WriteLine(modulo + "\n");
+                sw.Close();
+                
+            }
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog save = new SaveFileDialog();
+            save.Filter = "Images|*.png;";
+            save.FilterIndex = 1;
+            save.RestoreDirectory = true;
+
+            if (save.ShowDialog() == DialogResult.OK)
+            {
+
+                surface.Save(save.FileName, System.Drawing.Imaging.ImageFormat.Png);
+
+            }
+        }
+
+        private void loadToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.Filter = "Images|*.png;";
+            open.FilterIndex = 1;
+            open.RestoreDirectory = true;
+
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+
+                surface = new Bitmap(open.FileName);
+            }
+
+            pnlCanvas.Width = surface.Width;
+            pnlCanvas.Height = surface.Height;
+
+            pnlCanvas.BackgroundImage = surface;
+            graph = Graphics.FromImage(surface);
+            
+        }
+
+        private void btnExitApp_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+            this.Close();
+        }
+    }
+}
